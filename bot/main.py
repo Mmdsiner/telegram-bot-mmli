acbca2fefbaab30dd707fdd41745ecd5e4ffb8b8
Mmdsiner <mmdkingoff@gmail.com>
2026-04-28 21:14:26 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,13 @@
-import asyncio
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder
 from config import TOKEN
-from handlers.user import start, user_handler
+from handlers.user import user_handler
 from handlers.admin import admin_handler
-from database import init_db
 
-async def main():
-    app = ApplicationBuilder().token(TOKEN).build()
+app = ApplicationBuilder().token(TOKEN).build()
 
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(user_handler)
-    app.add_handler(admin_handler)
-
-    await init_db()
-    await app.run_polling()
+# اضافه کردن هندلرها
+app.add_handler(user_handler)
+app.add_handler(admin_handler)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run_polling()
